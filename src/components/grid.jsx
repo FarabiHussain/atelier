d3d48e0e87390c51b3dd4ba6804f0c3f1a0a8cd5
FarabiHussain <farabihussain8@gmail.com>
2023-10-20 00:26:17 -0500

    formatting

--- src/components/grid.jsx
+++ src/components/grid.jsx
@@ -1,18 +1,16 @@
-import { Container, Row, Col } from 'react-bootstrap';
-import Card from './card';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col } from "react-bootstrap";
+import Card from "./card";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function Grid(cards) {
 	return (
 		<Container fluid>
 			<Row xs={1} md={5}>
-				{
-					cards.list.map(
-						(card) => (
-							<Col><Card content={card} /></Col>
-						)
-					)
-				}
+				{cards.list.map((card) => (
+					<Col>
+						<Card content={card} />
+					</Col>
+				))}
 			</Row>
 		</Container>
 	);
